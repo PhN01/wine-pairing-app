f437c25fdf14d5a23f4b82b6e2baf2d78d794c0f
PhN01 <philipp.nikolaus90@gmail.com>
2022-06-16 23:40:06 +0200

    rename main file

--- app/main.py
+++ app/main.py
@@ -1,21 +1,52 @@
+import json
 import os
-from typing import Tuple
+from typing import Dict, List, Tuple
+import requests
 
 import pandas as pd
+import pydeck as pdk
 import streamlit as st
+from streamlit.delta_generator import DeltaGenerator
 from transformers.modeling_utils import PreTrainedModel
 from transformers.models.auto import AutoModel, AutoTokenizer
 from transformers.tokenization_utils import PreTrainedTokenizer
 
 import app.constants.constants as cons
 from app.utils.embedding_utils import get_best_wines
+from app.utils.gis_utils import get_largest_outer_ring_polygon
 
 st.set_page_config(page_title="Wine recommendations", layout="wide")
 
 
 @st.experimental_memo
-def load_data():
+def load_grape_data():
     df = pd.read_pickle(os.path.join(cons.DATA_PATH, cons.INPUT_FILE))
+    return df
+
+
+
+@st.experimental_memo
+def load_polygons():
+    # with open(os.path.join(cons.DATA_PATH, cons.POLYGON_FILE), "r") as f:
+    #     data = json.load(f)
+    data = pd.read_json(cons.POLYGON_FILE)
+    df = pd.DataFrame()
+    df["country"] = data.features.apply(lambda row: cons.COUNTRY_MAPPING.get(row["properties"]["admin"], row["properties"]["admin"]))
+    df["coordinates"] = data.features.apply(lambda row: get_largest_outer_ring_polygon(row["geometry"]["coordinates"]))
+    # df["coordinates"] = df.geometry.apply(lambda x: x["coordinates"])
+    # df["coordinates"] = df.coordinates.apply(lambda x: [[y[1], y[0]] for y in x[0]])
+    # df["lat"] = df.coordinates.apply(lambda x: [y[1] for y in x])
+    # df["lon"] = df.coordinates.apply(lambda x: [y[0] for y in x])
+    return df
+
+
+@st.experimental_memo
+def load_country_bd():
+    df = pd.read_csv(os.path.join(cons.DATA_PATH, cons.COUNTRY_FILE))
+    country_cols = list(df.columns)[2:]
+    df["country_list"] = df.apply(
+        lambda row: list(row[country_cols].loc[~row[country_cols].isnull()].index), axis=1
+    )
     return df
 
 
@@ -28,83 +59,198 @@ def load_model() -> Tuple[PreTrainedTokenizer, PreTrainedModel]:
 
 st.title("Wine recommendation engine")
 
-variety_df = load_data()
+variety_df = load_grape_data()
+polygon_df = load_polygons()
+country_bd_df = load_country_bd()
 tokenizer, model = load_model()
 
-# -------------------------------------------------------------------
-# Page layout
-# -------------------------------------------------------------------
 
+class App:
+    layout: Dict[str, DeltaGenerator]
 
-def create_wine_layout():
-    layout_dict = {}
-    cols = st.columns([2, 1, 1, 1, 1, 1, 1, 2])
-    for i, col in enumerate(cols):
-        with col:
-            layout_dict[i] = st.empty()
-    return layout_dict
+    def __init__(self) -> None:
+        self.layout = self.set_layout()
 
+    @staticmethod
+    def set_layout() -> Dict[str, DeltaGenerator]:
+        layout = {}
+        c1, c2, c3 = [st.container(), st.container(), st.empty()]
+        layout["header"] = c1
+        layout["main_input"] = c2
+        layout["main_content"] = c3
 
-def create_wine_list_layout(n_rows: int) -> dict:
-    rows = {i: st.container() for i in range(n_rows)}
-    layout_dict = {}
-    for i, r in rows.items():
-        with r:
-            layout_dict[i] = create_wine_layout()
-    return layout_dict
+        with layout["main_input"]:
+            # TODO: currently no option to align column contents, which
+            # results in buttons not being aligned with text
+            # streamlit team is working on it
+            text, buttons = st.columns([8, 2])
+            layout["input_text"] = text
+            layout["input_buttons"] = buttons
 
-# -------------------------------------------------------------------
-# Rendering helpers
-# -------------------------------------------------------------------
+        with layout["main_content"].container():
+            nav_col, space_col, cont1_col, cont2_col, cont3_col = st.columns(
+                [2.5, 0.2, 2, 2, 4]
+            )
+            layout["recommendations"] = nav_col
+            layout["profile"] = cont1_col
+            layout["flavors"] = cont2_col
+            layout["regions"] = cont3_col
 
+        return layout
 
-def profile_value_to_md(name: str, val: int) -> str:
-    return f"{name}<br> __{val}__/5"
+    def render_layout(self) -> None:
+        self._set_page_container_style()
 
+        s = st.session_state
+        if not s:
+            s.pressed_first_button = False
 
-def wine_falvors_to_md(wine_data: pd.Series):
-    return f"__Tastes like:__ {wine_data['flavor_0']}, {wine_data['flavor_1']} and {wine_data['flavor_2']}."
+        with self.layout["header"]:
+            st.markdown(
+                "We want to help you figure out, which wine works best with your meal plans. Tell us what you plant to eat:"
+            )
+        with self.layout["input_text"]:
+            text = st.text_area("")
+        with self.layout["input_buttons"]:
+            if st.button("Find wines") or s.pressed_first_button:
+                s.pressed_first_button = True
+                df = get_best_wines(
+                    sentence=text,
+                    ref_embeddings=variety_df[cons.MODEL_NAME],
+                    tokenizer=tokenizer,
+                    model=model,
+                    df=variety_df,
+                )
+                wines_list = df.name.head(3).tolist()
+                with self.layout["recommendations"]:
+                    st.subheader("Recommended Wines:")
+                    st.markdown(self._wines_to_md(wines_list))
+                    st.selectbox(
+                        "Select a wine to learn more...",
+                        key="wine_sel",
+                        options=[None] + wines_list,
+                        on_change=self._update_wine_detail,
+                        format_func=lambda x: x if not x is None else "--",
+                    )
+            if st.button("Clear outputs"):
+                self.layout["main_content"].empty()
 
+    @staticmethod
+    def _wines_to_md(wine_names: List[str]) -> str:
+        res = []
+        for wine in wine_names:
+            res.append(f"* {wine}")
+        return "\n".join(res)
 
-def render_wine(wine_data: pd.Series, layout_dict: dict) -> None:
-    layout_dict[0].markdown(f"### {wine_data['name']}")
-    layout_dict[1].metric("Confidence", wine_data["similarity"])
-    for i, item in enumerate(["Sweetness", "Body", "Tannins", "Acidity", "Alcohol"]):
-        layout_dict[i + 2].markdown(
-            profile_value_to_md(item, wine_data[f"{item.lower()}_value"]),
+    @staticmethod
+    def _profile_to_md(wine: pd.Series) -> str:
+        mapping = {
+            "sweetness_label": "Sweetness",
+            "body_label": "Body",
+            "tannins_label": "Tannins",
+            "acidity_label": "Acidity",
+            "alcohol_label": "Alcohol",
+        }
+        profile = wine.loc[mapping.keys()].rename(index=lambda x: mapping[x]).squeeze()
+        res = [f"**{k}:** {v}" for k, v in profile.items()]
+        return "<br>".join(res)
+
+    @staticmethod
+    def _flavors_to_md(wine: pd.Series) -> str:
+        flavor_cols = [f"flavor_{i}" for i in range(5)]
+
+        res = []
+        for flavor in wine.loc[flavor_cols].tolist():
+            res.append(f"* {flavor}")
+        return "\n".join(res)
+
+    @staticmethod
+    def _render_polygon_map(wine: pd.Series) -> None:
+        global country_bd_df
+        global polygon_df
+
+        country_bd = country_bd_df.loc[country_bd_df.name==wine["name"]].squeeze()
+        countries = country_bd["country_list"]
+        polygons = polygon_df.loc[polygon_df.country.isin(countries)]
+        polygons["prod_share"] = polygons.country.map(country_bd)
+
+        st.pydeck_chart(
+            pdk.Deck(
+                map_style='mapbox://styles/mapbox/light-v9',
+                initial_view_state=pdk.ViewState(
+                    latitude=19,
+                    longitude=21,
+                    zoom=0,
+                    pitch=0,
+                    # height="100%"
+                ),
+                # views=[pdk.View(type="MapView", controller=True, height="50%")],
+                tooltip={
+                    "html": "<b>Country:</b> {country} <br /><b>Share of global production:</b> {prod_share}%"
+                },
+                layers=[
+                    pdk.Layer(
+                        "PolygonLayer",
+                        polygons,
+                        id="geojson",
+                        opacity=0.8,
+                        stroked=False,
+                        get_polygon="coordinates",
+                        filled=True,
+                        extruded=False,
+                        wireframe=True,
+                        get_fill_color=[80, 176, 207],
+                        get_line_color=[80, 176, 207],
+                        auto_highlight=True,
+                        pickable=True,
+                    ),
+                ],
+            )
+        )
+
+    def _update_wine_detail(self):
+        global variety_df
+        wine_name = st.session_state.wine_sel
+        if wine_name is None:
+            self.layout["profile"].empty()
+            self.layout["flavors"].empty()
+            self.layout["regions"].empty()
+        else:
+            wine_data = variety_df.loc[variety_df.name == wine_name].squeeze()
+            with self.layout["profile"]:
+                st.subheader("Profile")
+                st.markdown(self._profile_to_md(wine_data), unsafe_allow_html=True)
+            with self.layout["flavors"]:
+                st.subheader("Tastes like...")
+                st.markdown(self._flavors_to_md(wine_data))
+            with self.layout["regions"]:
+                self.layout["regions"].empty()
+                st.subheader("Where this wine is grown:")
+                self._render_polygon_map(wine_data)
+
+    @staticmethod
+    def _set_page_container_style() -> None:
+        st.markdown(
+            """
+            <style>
+                .css-18e3th9 {
+                        padding-top: 1.5rem;
+                        padding-bottom: 10rem;
+                        padding-left: 5rem;
+                        padding-right: 5rem;
+                    }
+                .css-1d391kg {
+                        padding-top: 3.5rem;
+                        padding-right: 1rem;
+                        padding-bottom: 3.5rem;
+                        padding-left: 1rem;
+                    }
+            </style>
+            """,
             unsafe_allow_html=True,
         )
-    layout_dict[7].markdown(wine_falvors_to_md(wine_data))
 
 
-def clear_wine_list(layout_dict: dict) -> None:
-    for el in layout_dict.values():
-        el.empty()
+app = App()
 
-# -------------------------------------------------------------------
-# Render site
-# -------------------------------------------------------------------
-
-
-c1, c2, c3 = [st.container(), st.container(), st.container()]
-with c1:
-    text = st.text_area("What are you eating?")
-with c3:
-    wines_layout = create_wine_list_layout(cons.NUM_WINES)
-with c2:
-    col1, col2, col3 = st.columns([3, 1, 7])
-    with col1:
-        if st.button("Find wines..."):
-            df = get_best_wines(
-                sentence=text,
-                ref_embeddings=variety_df[cons.MODEL_NAME],
-                tokenizer=tokenizer,
-                model=model,
-                df=variety_df,
-            )
-            for i in range(3):
-                render_wine(df.iloc[i], wines_layout[i])
-    with col2:
-        if st.button("Clear"):
-            for i in range(3):
-                clear_wine_list(wines_layout[i])
+app.render_layout()
