3198ca8c14f4c8282a5a73602987f8cb0d3f38a3
PhN01 <philipp.nikolaus90@gmail.com>
2022-06-16 23:38:10 +0200

    update constants

--- app/constants/constants.py
+++ app/constants/constants.py
@@ -1,5 +1,11 @@
 DATA_PATH = "data"
 INPUT_FILE = "wine_recommendation_input.pkl"
+POLYGON_FILE = "https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_110m_admin_0_countries.geojson"
+COUNTRY_FILE = "20220615_winesearcher_winefolly_grape_regions.csv"
 MODEL_NAME = "nreimers/albert-small-v2"
 
 NUM_WINES = 3
+
+COUNTRY_MAPPING = {
+    "United States of America": "USA"
+}
